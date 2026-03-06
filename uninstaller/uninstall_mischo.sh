52fef9fc765799f8591d4a54dd1654f9e7e55843
DarkFox <darkfox.tobias@outlook.com>
2026-03-06 12:54:59 +0100

    Update uninstall_mischo.sh

--- uninstaller/uninstall_mischo.sh
+++ uninstaller/uninstall_mischo.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+read -p "Are you sure you want to uninstall Mischo? (Y/N): " CONFIRM
+[[ "$CONFIRM" =~ ^[Yy]$ ]] || exit 1
 
+systemctl --user stop mischo 2>/dev/null
+systemctl --user disable mischo 2>/dev/null
+rm -f ~/.config/systemd/user/mischo.service
+systemctl --user daemon-reload
+rm -rf ~/.mischo
+echo "Mischo uninstalled."
